b7064fe57410de552f989f082f5c9956f4cbd97e
Zhenmao Wan <wanzhenmao@gmail.com>
2026-01-22 20:50:28 +0800

    Simplify hero section animation

--- script.js
+++ script.js
@@ -1,64 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const stickySections = document.querySelectorAll("[data-sticky]");
-  const stickyTexts = document.querySelectorAll("[data-sticky] h2");
+  gsap.registerPlugin(Flip);
 
-  if (window.innerWidth < 960) {
-    stickySections.forEach((stickySection) => stickySection.remove());
-  } else {
-    gsap.registerPlugin(ScrollTrigger);
+  const hero = document.querySelector(".hero");
+  const heroTexts = document.querySelectorAll(".hero__text");
 
-    const lenis = new Lenis();
-    lenis.on("scroll", ScrollTrigger.update);
-    gsap.ticker.add((time) => lenis.raf(time * 1000));
-    gsap.ticker.lagSmoothing(0);
+  const tl = gsap.timeline({
+    delay: 0.5,
+    onComplete: () => {
+      const state = Flip.getState(heroTexts);
+      hero.classList.add("align-start");
+      Flip.from(state, { duration: 0.5, ease: "power2.inOut" });
+    },
+  });
 
-    let targetScales = Array({ length: stickySections.length }).fill(1);
-    function calculateTargetScale(stickyIndex) {
-      const stickySection = stickySections[stickyIndex - 1];
-      const stickyText = stickyTexts[stickyIndex - 1];
+  tl.set(hero, {
+    autoAlpha: 1,
+    yPercent: 66,
+  });
 
-      const sectionHeight = stickySection.offsetHeight;
-      const textHeight = stickyText.offsetHeight;
-
-      targetScales[stickyIndex - 1] = sectionHeight / textHeight;
-    }
-
-    const ro = new ResizeObserver((entries) =>
-      entries.forEach((entry) => {
-        const stickyIndex = +entry.target.dataset.sticky;
-        calculateTargetScale(stickyIndex);
-      })
-    );
-    stickySections.forEach((stickySection) => ro.observe(stickySection));
-
-    function setScaleY(element, scale) {
-      element.style.transform = `scaleY(${scale})`;
-    }
-
-    stickyTexts.forEach((stickyText, i) => {
-      ScrollTrigger.create({
-        trigger: stickyText,
-        start: "top bottom",
-        end: "top top",
-        scrub: 1,
-        onUpdate: (self) => {
-          const scale = targetScales[i] * self.progress;
-          setScaleY(stickyText, scale);
-        },
-      });
-
-      ScrollTrigger.create({
-        trigger: stickyText,
-        start: "top top",
-        end: `+=${window.innerHeight}px`,
-        pin: true,
-        pinSpacing: false,
-        scrub: 1,
-        onUpdate: (self) => {
-          const scale = targetScales[i] * (1 - self.progress);
-          setScaleY(stickyText, scale);
-        },
-      });
+  heroTexts.forEach((text, index) => {
+    tl.from(text, {
+      yPercent: 120,
+      opacity: 0,
+      scale: 0.5,
+      duration: 0.5,
+      ease: "power3",
     });
-  }
+    if (index > 0) {
+      tl.to(
+        hero,
+        {
+          yPercent: "-=33",
+          duration: 0.5,
+          ease: "power2",
+        },
+        "<",
+      );
+    }
+  });
 });
